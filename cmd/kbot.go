6e2bbd8c345f1ce573b8991dbcf1e82c9eb750ea
Tsisar <fhantom.tp@gmail.com>
2025-05-11 23:09:56 +0400

    Implement telebot

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,19 +1,25 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"gopkg.in/telebot.v3"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
+var Token = os.Getenv("TELEGRAM_TOKEN")
+
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
-	Short: "A brief description of your command",
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +27,38 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kbot called")
+		log.Printf("kbot %s called", appVersion)
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  Token,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			log.Fatalf("error creating bot: %v", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Printf(m.Message().Payload, m.Text())
+			payload := m.Text()
+
+			switch payload {
+			case "hello":
+				if err := m.Send(fmt.Sprintf("Hello, %s!\nI'm Kbot %s", m.Message().Sender.FirstName, appVersion)); err != nil {
+					log.Printf("error replying to message: %v", err)
+					return err
+				}
+			default:
+				if err := m.Send(fmt.Sprintf("Сам ти %s!", payload)); err != nil {
+					log.Printf("error replying to message: %v", err)
+					return err
+				}
+			}
+
+			return err
+		})
+
+		kbot.Start()
 	},
 }
 
